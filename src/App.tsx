622d41433612f6b92665c69fe8973d8a4395657d
self <self>
2020-10-25 16:02:12 +0900

    react-router用にコード修正（/をreactのデフォルトページへ変更する）

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-import { MyComponent } from 'components/component';
+import { ReactDefault } from 'container/ReactDefault';
 import React from 'react';
+import {
+  BrowserRouter as Router, Route, Switch
+} from 'react-router-dom';
 import './App.css';
-import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <MyComponent foo="aaaa" />
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/"><ReactDefault /></Route>
+      </Switch>
+    </Router>
   );
 }
 
